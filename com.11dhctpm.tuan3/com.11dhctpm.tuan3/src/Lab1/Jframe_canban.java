7eb585e80407934af8b5cfbdf22117c113324c0c
nguyenducmanh1010 <55130687+nguyenducmanh1010@users.noreply.github.com>
2019-09-29 13:18:24 +0700

    BaiTapTuan3

---
+++ com.11dhctpm.tuan3/com.11dhctpm.tuan3/src/Lab1/Jframe_canban.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Lab1;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author USER
+ */
+public class Jframe_canban extends JFrame {
+    public Jframe_canban(){
+        setVisible(true);
+        setTitle("String");        
+        setSize(400, 400);
+    }
+    public static void main(String[] args){
+        new Jframe_canban();
+    }
+    
+    
+}
